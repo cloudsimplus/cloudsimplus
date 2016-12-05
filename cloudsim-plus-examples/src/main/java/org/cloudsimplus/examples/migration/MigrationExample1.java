5930bb5e78cb1ffa9b85175641109358871b46a1
Manoel Campos <manoelcampos@gmail.com>
2016-12-05 13:37:11 +0000

    Used the com.mycila.maven-license-plugin to add license information for CloudSim Plus exclusive source files

--- cloudsim-plus-examples/src/main/java/org/cloudsimplus/examples/migration/MigrationExample1.java
+++ cloudsim-plus-examples/src/main/java/org/cloudsimplus/examples/migration/MigrationExample1.java
@@ -1,10 +1,23 @@
-/*
- * Title:        CloudSim Toolkit
- * Description:  CloudSim (Cloud Simulation) Toolkit for Modeling and Simulation
- *               of Clouds
- * Licence:      GPL - http://www.gnu.org/copyleft/gpl.html
+/**
+ * CloudSim Plus: A highly-extensible and easier-to-use Framework for Modeling and Simulation of Cloud Computing Infrastructures and Services.
+ * http://cloudsimplus.org
  *
- * Copyright (c) 2009, The University of Melbourne, Australia
+ *     Copyright (C) 2015-2016  Universidade da Beira Interior (UBI, Portugal) and the Instituto Federal de Educação Ciência e Tecnologia do Tocantins (IFTO, Brazil).
+ *
+ *     This file is part of CloudSim Plus.
+ *
+ *     CloudSim Plus is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     CloudSim Plus is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with CloudSim Plus. If not, see <http://www.gnu.org/licenses/>.
  */
 package org.cloudsimplus.examples.migration;
 
