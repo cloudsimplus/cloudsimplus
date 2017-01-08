b123165ef1c20d6182e32ddad284b03fd2e1f04a
Manoel Campos <manoelcampos@gmail.com>
2017-01-08 13:29:00 +0000

    Updated docs config

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,8 @@
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.coverage', 'javasphinx']
+#'sphinx.ext.coverage', 'sphinx.ext.imgmath', 
+extensions = ['javasphinx']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
