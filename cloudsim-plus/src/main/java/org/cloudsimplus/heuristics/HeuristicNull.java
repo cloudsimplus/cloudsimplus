ea31795539fda7c89986660188d270b38415abe7
Manoel Campos <manoelcampos@gmail.com>
2017-04-01 02:09:54 +0100

    Fix build error

--- cloudsim-plus/src/main/java/org/cloudsimplus/heuristics/HeuristicNull.java
+++ cloudsim-plus/src/main/java/org/cloudsimplus/heuristics/HeuristicNull.java
@@ -6,7 +6,7 @@ package org.cloudsimplus.heuristics;
  *
  * @author Manoel Campos da Silva Filho
  */
-final class HeuristicNull<S extends HeuristicSolution<?>> implements Heuristic<S> {
+class HeuristicNull<S extends HeuristicSolution<?>> implements Heuristic<S> {
     @Override public double getAcceptanceProbability() { return 0.0; }
 	@Override public int getRandomValue(int maxValue) { return 0; }
 	@Override public boolean isToStopSearch() { return false; }
