65fbf3b7cf67ee76c94fe561313f8cc1d889acd0
Manoel Campos <manoelcampos@gmail.com>
2017-05-28 08:06:55 +0100

    Updates version and changelog to release CloudSim Plus 1.2.1.

--- cloudsim-plus/src/main/java/org/cloudbus/cloudsim/core/CloudSim.java
+++ cloudsim-plus/src/main/java/org/cloudbus/cloudsim/core/CloudSim.java
@@ -35,7 +35,7 @@ public class CloudSim implements Simulation {
     /**
      * CloudSim Plus current version.
      */
-    public static final String VERSION = "1.2.0";
+    public static final String VERSION = "1.2.1";
 
     /**
      * A constant to indicate that some entity was not found.
