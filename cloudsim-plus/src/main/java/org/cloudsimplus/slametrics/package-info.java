c20b2881f15314c7442742e120d3640cde8dbcc5
Manoel Campos da Silva Filho <manoelcampos@gmail.com>
2017-06-03 16:55:35 +0100

    Update package-info.java

--- cloudsim-plus/src/main/java/org/cloudsimplus/slametrics/package-info.java
+++ cloudsim-plus/src/main/java/org/cloudsimplus/slametrics/package-info.java
@@ -1,7 +1,7 @@
 /**
- * Provides classes to load SLA contracts from JSON files
+ * Provides classes to load SLA contracts from JSON files,
  * according to the format defined by the
- * <a href="http://docs.aws.amazon.com/AmazonCloudWatch/latest/monitoring/viewing_metrics_with_cloudwatch.html">Amazon Cloudwatch</a>.
+ * <a href="http://docs.aws.amazon.com/AmazonCloudWatch/latest/monitoring/viewing_metrics_with_cloudwatch.html">AWS Cloudwatch</a>.
  *
  * <p>A JSON file can be read using a method such as the
  * {@link org.cloudsimplus.slametrics.SlaContract#getInstance(java.lang.String)}.</p>
