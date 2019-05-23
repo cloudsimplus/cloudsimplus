cef79398c9b62a8467218b60a0f3cf1306155353
Manoel Campos <manoelcampos@gmail.com>
2019-05-23 16:44:13 -0300

    Updates VmAllocationPolicyFirstFit and introduces VmAllocationPolicyRoundRobin.
    = VmAllocationPolicyFirstFit
    
    The VmAllocationPolicyFirstFit now defines the max number of tries to find a Host.
    The number of iterations of the loop is defined
    as the number of Hosts, but as soon a suitable Host is found,
    the method returns immediately.
    
    If the lastHostIndex is greater than 0 when the loop starts and
    the end of the Host List is reached without finding a suitable Host,
    the small change enables the policy to start from the beginning of the
    list a keep trying to find a Host, until the number of iterations
    reaches the number of Hosts.
    
    = VmAllocationPolicyRoundRobin
    
    This new policy follows the same approach presented above,
    defining the max number of tries to find a suitable Host,
    however it always moves to the next Hosts after selecting a suitable one.
    
    Signed-off-by: Manoel Campos <manoelcampos@gmail.com>

--- cloudsim-plus/src/main/java/org/cloudbus/cloudsim/allocationpolicies/VmAllocationPolicyRoundRobin.java
+++ cloudsim-plus/src/main/java/org/cloudbus/cloudsim/allocationpolicies/VmAllocationPolicyRoundRobin.java
@@ -7,8 +7,10 @@ import java.util.List;
 import java.util.Optional;
 
 /**
- * A <b>First Fit VM allocation policy</b>
- * which finds the first Host having suitable resources to place a given VM.
+ * A <b>Round-Robin VM allocation policy</b>
+ * which finds the next Host having suitable resources to place a given VM
+ * in a circular way. That means when it selects a suitable Host to place a VM,
+ * it moves to the next suitable Host when a new VM has to be placed.
  * This is a high time-efficient policy with a best-case complexity O(1)
  * and a worst-case complexity O(N), where N is the number of Hosts.
  *
@@ -16,16 +18,18 @@ import java.util.Optional;
  *     <b>NOTES:</b>
  *     <ul>
  *         <li>This policy doesn't perform optimization of VM allocation by means of VM migration.</li>
- *         <li>It has a low computational complexity but may return
+ *         <li>It has a low computational complexity (high time-efficient) but may return
  *         and inactive Host that will be activated, while there may be active Hosts
  *         suitable for the VM.</li>
+ *         <li>Despite the low computational complexity, such a policy will increase the number of active Hosts,
+ *         that increases power consumption.</li>
  *     </ul>
  * </p>
  *
  * @author Manoel Campos da Silva Filho
- * @since CloudSim Plus 1.0.0
+ * @since CloudSim Plus 4.4.2
  */
-public class VmAllocationPolicyFirstFit extends VmAllocationPolicyAbstract implements VmAllocationPolicy {
+public class VmAllocationPolicyRoundRobin extends VmAllocationPolicyAbstract implements VmAllocationPolicy {
     /**
      * The index of the last host used to place a VM.
      */
@@ -39,15 +43,11 @@ public class VmAllocationPolicyFirstFit extends VmAllocationPolicyAbstract imple
         final int maxTries = hostList.size();
         for (int i = 0; i < maxTries; i++) {
             final Host host = hostList.get(lastHostIndex);
+            //Different from the FirstFit policy, it always increments the host index.
+            lastHostIndex = ++lastHostIndex % hostList.size();
             if (host.isSuitableForVm(vm)) {
                 return Optional.of(host);
             }
-
-            /* If it gets here, the previous Host doesn't have capacity to place the VM.
-             * Then, moves to the next Host.
-             * If the end of the Host list is reached, starts from the beginning,
-             * until the .*/
-            lastHostIndex = ++lastHostIndex % hostList.size();
         }
 
         return Optional.empty();
