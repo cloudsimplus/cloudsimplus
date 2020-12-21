5818bb5fbd639559a3e2f3cb352e211361f652e7
Manoel Campos <manoelcampos@gmail.com>
2020-12-21 11:32:33 -0300

    Adds private constructor to Util class.
    Signed-off-by: Manoel Campos <manoelcampos@gmail.com>

--- cloudsim-plus/src/main/java/org/cloudbus/cloudsim/util/Util.java
+++ cloudsim-plus/src/main/java/org/cloudbus/cloudsim/util/Util.java
@@ -6,6 +6,9 @@ package org.cloudbus.cloudsim.util;
  * @since CloudSim Plus 6.0.0
  */
 public final class Util {
+    /** A private default constructor to avoid class instantiation. */
+    private Util(){}
+
     public static void sleep(final long millis) {
         try {
             Thread.sleep(millis);
