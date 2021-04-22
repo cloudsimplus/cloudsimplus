3a8a65b7ac462fe22ebe175aa8813fa4dd38fedf
Manoel Campos <manoelcampos@gmail.com>
2021-04-22 19:02:03 -0300

    Refactoring.
    Signed-off-by: Manoel Campos <manoelcampos@gmail.com>

--- cloudsim-plus/src/main/java/org/cloudbus/cloudsim/core/events/CloudSimEvent.java
+++ cloudsim-plus/src/main/java/org/cloudbus/cloudsim/core/events/CloudSimEvent.java
@@ -218,7 +218,7 @@ public final class CloudSimEvent implements SimEvent {
     }
 
     @Override
-    public boolean equals(Object o) {
+    public boolean equals(final Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         final CloudSimEvent that = (CloudSimEvent) o;
