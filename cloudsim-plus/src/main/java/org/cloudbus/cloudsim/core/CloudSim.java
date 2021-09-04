b5070456d756c9d7828ddfbb8db705a83e548310
Manoel Campos <manoelcampos@gmail.com>
2021-09-04 20:43:03 -0300

    Version bump to 6.4.4
    Signed-off-by: Manoel Campos <manoelcampos@gmail.com>

--- cloudsim-plus/src/main/java/org/cloudbus/cloudsim/core/CloudSim.java
+++ cloudsim-plus/src/main/java/org/cloudbus/cloudsim/core/CloudSim.java
@@ -38,7 +38,7 @@ public class CloudSim implements Simulation {
     /**
      * CloudSim Plus current version.
      */
-    public static final String VERSION = "CloudSim Plus 6.4.3";
+    public static final String VERSION = "CloudSim Plus 6.4.4";
 
     public static final Logger LOGGER = LoggerFactory.getLogger(CloudSim.class.getSimpleName());
 
