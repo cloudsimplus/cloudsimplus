f075143f2cfc2faca11d879e0afcd7f4f67f828f
Manoel Campos <manoelcampos@gmail.com>
2021-09-25 18:34:18 -0300

    Update docs
    Signed-off-by: Manoel Campos <manoelcampos@gmail.com>

--- cloudsim-plus/src/main/java/org/cloudbus/cloudsim/power/package-info.java
+++ cloudsim-plus/src/main/java/org/cloudbus/cloudsim/power/package-info.java
@@ -1,8 +1,9 @@
 /**
  * Provides classes and interfaces for the power-consumption module.
- * There are interfaces representing objects having power capabilities
- * ({@link org.cloudbus.cloudsim.power.PowerAware}),
- * classes that model how physical or even virtual elements
- * consume power or even classes to enable collecting power consumption data.
+ * There are used to:
+ * (i) represent objects having power capabilities
+ *     ({@link org.cloudbus.cloudsim.power.PowerAware});
+ * (ii) model how physical/virtual elements consume power; or
+ * (iii) enable collecting power consumption data.
  */
 package org.cloudbus.cloudsim.power;
