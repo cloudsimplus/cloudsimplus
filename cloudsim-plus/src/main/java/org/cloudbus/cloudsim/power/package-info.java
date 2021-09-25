c7c3b01fe8c546a3b96a1feabf60f27b21146a29
Manoel Campos <manoelcampos@gmail.com>
2021-09-25 18:09:19 -0300

    Adds package-info.java to org.cloudbus.cloudsim.power
    Signed-off-by: Manoel Campos <manoelcampos@gmail.com>

---
+++ cloudsim-plus/src/main/java/org/cloudbus/cloudsim/power/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Provides classes and interfaces for the power-consumption module.
+ * There are interfaces representing objects having power capabilities
+ * ({@link org.cloudbus.cloudsim.power.PowerAware}),
+ * classes that model how physical or even virtual elements
+ * consume power or even classes to enable collecting power consumption data.
+ */
+package org.cloudbus.cloudsim.power;
