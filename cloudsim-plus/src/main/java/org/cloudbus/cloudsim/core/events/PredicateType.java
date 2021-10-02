74f14c308a963b313b951a6f2acf0b2a62f3e313
Manoel Campos <manoelcampos@gmail.com>
2021-10-01 22:55:55 -0300

    Convert PredicateType to record
    Signed-off-by: Manoel Campos <manoelcampos@gmail.com>

--- cloudsim-plus/src/main/java/org/cloudbus/cloudsim/core/events/PredicateType.java
+++ cloudsim-plus/src/main/java/org/cloudbus/cloudsim/core/events/PredicateType.java
@@ -5,37 +5,26 @@
  *
  * Copyright (c) 2009-2012, The University of Melbourne, Australia
  */
-
 package org.cloudbus.cloudsim.core.events;
 
 import java.util.function.Predicate;
 
 /**
- * A predicate to select events with specific {@link SimEvent#getTag() tag}.
+ * A predicate to select events with a specific {@link SimEvent#getTag() tag}.
  *
  * @author Marcos Dias de Assuncao
+ * @param tag tag to match events
  * @see Predicate
  * @since CloudSim Toolkit 1.0
  */
-public class PredicateType implements Predicate<SimEvent> {
-    /** The {@link SimEvent#getTag() tag} to match events. */
-    private final int tag;
+public record PredicateType(int tag) implements Predicate<SimEvent> {
 
     /**
-     * Constructor used to select events with the given tag value.
+     * Matches any event that has the specified {@link #tag}.
      *
-     * @param tag an event {@link SimEvent#getTag() tag} value
-     */
-    public PredicateType(final int tag) {
-        this.tag = tag;
-    }
-
-    /**
-     * Matches any event that has one of the specified {@link #tag}.
-     *
-     * @param evt {@inheritDoc}
+     * @param evt the event to math with the specified {@link #tag()}
      * @return {@inheritDoc}
-     * @see #tag
+     * @see #tag()
      */
     @Override
     public boolean test(final SimEvent evt) {
