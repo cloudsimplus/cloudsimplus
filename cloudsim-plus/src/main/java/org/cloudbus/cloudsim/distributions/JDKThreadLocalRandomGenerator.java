6e8861192e6337fcd4334b3aab01104b14583c50
Manoel Campos <manoelcampos@gmail.com>
2021-10-02 00:59:01 -0300

    Adds license header to JDKThreadLocalRandomGenerator
    Signed-off-by: Manoel Campos <manoelcampos@gmail.com>

--- cloudsim-plus/src/main/java/org/cloudbus/cloudsim/distributions/JDKThreadLocalRandomGenerator.java
+++ cloudsim-plus/src/main/java/org/cloudbus/cloudsim/distributions/JDKThreadLocalRandomGenerator.java
@@ -1,3 +1,26 @@
+/*
+ * CloudSim Plus: A modern, highly-extensible and easier-to-use Framework for
+ * Modeling and Simulation of Cloud Computing Infrastructures and Services.
+ * http://cloudsimplus.org
+ *
+ *     Copyright (C) 2015-2021 Universidade da Beira Interior (UBI, Portugal) and
+ *     the Instituto Federal de Educação Ciência e Tecnologia do Tocantins (IFTO, Brazil).
+ *
+ *     This file is part of CloudSim Plus.
+ *
+ *     CloudSim Plus is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     CloudSim Plus is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with CloudSim Plus. If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.cloudbus.cloudsim.distributions;
 
 import org.apache.commons.math3.random.RandomGenerator;
@@ -6,8 +29,8 @@ import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * A {@link RandomGenerator} that internally uses the {@link ThreadLocalRandom},
- * a very fast Pseudo-Random Number Generator (PRNG) with higher performance than {@link java.util.Random},
- * mainly in concurrent environments.
+ * a very fast Pseudo-Random Number Generator (PRNG)
+ * with higher performance than {@link java.util.Random}, mainly in concurrent environments.
  * The {@link ThreadLocalRandom} also has
  * much better performance than PRNGs available in {@link org.apache.commons.math3},
  * despite it probably has worse statistical properties.
@@ -27,7 +50,8 @@ import java.util.concurrent.ThreadLocalRandom;
  * Classes such as {@link UniformDistr} provide such a feature if the
  * underlying PRNG allows setting a seed.
  * That is explained because the technique is applied when multiple runs of the same simulation
- * are executed. In such scenario, the second half of experiments have to use the seeds from the first half.
+ * are executed.
+ * In such scenario, the second half of experiments have to use the seeds from the first half.
  * </p>
  *
  * @author Manoel Campos da Silva Filho
