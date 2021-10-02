574b36b5a924c9aeed78066aaad2fb6e85ed7758
Manoel Campos <manoelcampos@gmail.com>
2021-10-01 23:17:49 -0300

    Updates UniquelyIdentifiable docs
    Signed-off-by: Manoel Campos <manoelcampos@gmail.com>

--- cloudsim-plus/src/main/java/org/cloudbus/cloudsim/core/UniquelyIdentifiable.java
+++ cloudsim-plus/src/main/java/org/cloudbus/cloudsim/core/UniquelyIdentifiable.java
@@ -26,7 +26,7 @@ package org.cloudbus.cloudsim.core;
 import org.cloudbus.cloudsim.brokers.DatacenterBroker;
 
 /**
- * An interface for objects that have an Unique Identifier (UID)
+ * An interface for objects that have a Unique Identifier (UID)
  * that is compounded by a {@link DatacenterBroker} ID
  * and the object ID.
  *
@@ -36,7 +36,7 @@ public interface UniquelyIdentifiable extends Identifiable {
     /**
      * Generates an Unique Identifier (UID).
      *
-     * @param brokerId the id of the {@link DatacenterBroker} (user)
+     * @param brokerId the id of the {@link DatacenterBroker}
      * @param id the object id
      * @return the generated UID
      */
@@ -45,9 +45,8 @@ public interface UniquelyIdentifiable extends Identifiable {
     }
 
     /**
-     * Gets the Unique Identifier (UID) for the VM, that is compounded by the id
-     * of a {@link DatacenterBroker} (representing the User)
-     * and the object id.
+     * Gets the Unique Identifier (UID) for the entity, that is compounded by the id
+     * of a {@link DatacenterBroker} and the object id.
      *
      * @return
      */
