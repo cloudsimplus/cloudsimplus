391929bd5176b0b756fea68dca98ba2ecd8e13f8
Manoel Campos <manoelcampos@gmail.com>
2021-10-04 15:43:31 -0300

    Update ResourceScalingInstantaneous docs
    Signed-off-by: Manoel Campos <manoelcampos@gmail.com>

--- cloudsim-plus/src/main/java/org/cloudsimplus/autoscaling/resources/ResourceScalingInstantaneous.java
+++ cloudsim-plus/src/main/java/org/cloudsimplus/autoscaling/resources/ResourceScalingInstantaneous.java
@@ -30,20 +30,21 @@ import org.cloudsimplus.autoscaling.VerticalVmScaling;
 import java.util.function.Function;
 
 /**
- * A {@link ResourceScaling} for which the capacity of the resource to be scaled will be instantaneously
- * resized to move the Vm from the under or overload state.
+ * A {@link ResourceScaling} for which the capacity of the resource to be scaled will be
+ * instantaneously resized to move the Vm from the under or overload state.
  * This way, the SLA violation time will be reduced.
  *
  * <p>This scaling type will resize the resource capacity in the following way:
  * <ul>
- *     <li>in underload conditions: it decreases the resource capacity to be equal to the current load of the resource
- *     being scaled;</li>
- *     <li>in overload conditions: it increases the resource capacity to be equal to the current load of the resource
- *     being scaled.</li>
+ *     <li>in underload conditions: it decreases the resource capacity to be equal to
+ *     the current load of the resource being scaled;</li>
+ *     <li>in overload conditions: it increases the resource capacity to be equal to
+ *     the current load of the resource being scaled.</li>
  * </ul>
  *
- * Finally it adds an extra amount of resource, defined by the {@link VerticalVmScaling#getScalingFactor() scaling factor},
- * for safety. This extra amount added is to enable the resource usage to grow up to the scaling factor
+ * Finally it adds an extra amount of resource, defined by the
+ * {@link VerticalVmScaling#getScalingFactor() scaling factor}, for safety.
+ * This extra amount added is to enable the resource usage to grow up to the scaling factor
  * without needing to resize the resource again. If it grows up to the scaling factor,
  * a new up scaling request will be sent.
  * </p>
