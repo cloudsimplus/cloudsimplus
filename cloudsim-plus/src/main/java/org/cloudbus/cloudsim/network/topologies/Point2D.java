557163f5bd14cc61bee3fc3329f6688eaf24cda1
Manoel Campos <manoelcampos@gmail.com>
2021-10-04 10:12:07 -0300

    Converts Point2D to record
    Signed-off-by: Manoel Campos <manoelcampos@gmail.com>

--- cloudsim-plus/src/main/java/org/cloudbus/cloudsim/network/topologies/Point2D.java
+++ cloudsim-plus/src/main/java/org/cloudbus/cloudsim/network/topologies/Point2D.java
@@ -2,28 +2,15 @@ package org.cloudbus.cloudsim.network.topologies;
 
 /**
  * A class to represent the coordinates of a 2-dimensional point.
+ * @param x horizontal coordinate
+ * @param y vertical coordinate
  */
-public class Point2D {
-    private final int x;
-    private final int y;
-
+public record Point2D(int x, int y) {
     /**
      * Creates an origin point with coordinates 0,0.
      */
     public Point2D(){
         this(0,0);
-    }
-
-    public Point2D(final int x, final int y){
-        this.x = x;
-        this.y = y;
-    }
-
-    public int getX() {
-        return x;
-    }
-    public int getY() {
-        return y;
     }
 
     @Override
