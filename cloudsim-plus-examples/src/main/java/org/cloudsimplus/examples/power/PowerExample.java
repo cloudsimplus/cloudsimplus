31482afa9e595c924b1f07bbb5319deba6141f7b
Manoel Campos <manoelcampos@gmail.com>
2021-10-27 09:15:16 -0300

    PowerExample refactoring
    Signed-off-by: Manoel Campos <manoelcampos@gmail.com>

--- cloudsim-plus-examples/src/main/java/org/cloudsimplus/examples/power/PowerExample.java
+++ cloudsim-plus-examples/src/main/java/org/cloudsimplus/examples/power/PowerExample.java
@@ -24,7 +24,6 @@
 package org.cloudsimplus.examples.power;
 
 import ch.qos.logback.classic.Level;
-import org.cloudbus.cloudsim.allocationpolicies.VmAllocationPolicySimple;
 import org.cloudbus.cloudsim.brokers.DatacenterBroker;
 import org.cloudbus.cloudsim.brokers.DatacenterBrokerSimple;
 import org.cloudbus.cloudsim.cloudlets.Cloudlet;
@@ -35,17 +34,11 @@ import org.cloudbus.cloudsim.datacenters.DatacenterSimple;
 import org.cloudbus.cloudsim.hosts.Host;
 import org.cloudbus.cloudsim.hosts.HostSimple;
 import org.cloudbus.cloudsim.power.models.PowerModel;
-import org.cloudbus.cloudsim.power.models.PowerModelHost;
 import org.cloudbus.cloudsim.power.models.PowerModelHostSimple;
-import org.cloudbus.cloudsim.provisioners.PeProvisionerSimple;
-import org.cloudbus.cloudsim.provisioners.ResourceProvisioner;
-import org.cloudbus.cloudsim.provisioners.ResourceProvisionerSimple;
 import org.cloudbus.cloudsim.resources.Pe;
 import org.cloudbus.cloudsim.resources.PeSimple;
 import org.cloudbus.cloudsim.schedulers.cloudlet.CloudletSchedulerTimeShared;
-import org.cloudbus.cloudsim.schedulers.vm.VmScheduler;
 import org.cloudbus.cloudsim.schedulers.vm.VmSchedulerTimeShared;
-import org.cloudbus.cloudsim.utilizationmodels.UtilizationModel;
 import org.cloudbus.cloudsim.utilizationmodels.UtilizationModelDynamic;
 import org.cloudbus.cloudsim.utilizationmodels.UtilizationModelFull;
 import org.cloudbus.cloudsim.vms.HostResourceStats;
@@ -154,7 +147,7 @@ public class PowerExample {
 
         simulation = new CloudSim();
         hostList = new ArrayList<>(HOSTS);
-        datacenter0 = createDatacenterSimple();
+        datacenter0 = createDatacenter();
         //Creates a broker that is a software acting on behalf of a cloud customer to manage his/her VMs and Cloudlets
         broker0 = new DatacenterBrokerSimple(simulation);
 
@@ -209,7 +202,7 @@ public class PowerExample {
     private void printVmsCpuUtilizationAndPowerConsumption() {
         vmList.sort(comparingLong(vm -> vm.getHost().getId()));
         for (Vm vm : vmList) {
-            final PowerModelHost powerModel = vm.getHost().getPowerModel();
+            final var powerModel = vm.getHost().getPowerModel();
             final double hostStaticPower = powerModel instanceof PowerModelHostSimple powerModelHost ? powerModelHost.getStaticPower() : 0;
             final double hostStaticPowerByVm = hostStaticPower / vm.getHost().getVmCreatedList().size();
 
@@ -250,44 +243,38 @@ public class PowerExample {
     /**
      * Creates a {@link Datacenter} and its {@link Host}s.
      */
-    private Datacenter createDatacenterSimple() {
+    private Datacenter createDatacenter() {
         for(int i = 0; i < HOSTS; i++) {
-            Host host = createPowerHost(i);
+            final var host = createPowerHost(i);
             hostList.add(host);
         }
 
-        final Datacenter dc = new DatacenterSimple(simulation, hostList, new VmAllocationPolicySimple());
+        final var dc = new DatacenterSimple(simulation, hostList);
         dc.setSchedulingInterval(SCHEDULING_INTERVAL);
         return dc;
     }
 
     private Host createPowerHost(final int id) {
-        final List<Pe> peList = new ArrayList<>(HOST_PES);
+        final var peList = new ArrayList<Pe>(HOST_PES);
         //List of Host's CPUs (Processing Elements, PEs)
         for (int i = 0; i < HOST_PES; i++) {
-            peList.add(new PeSimple(1000, new PeProvisionerSimple()));
+            peList.add(new PeSimple(1000));
         }
 
         final long ram = 2048; //in Megabytes
         final long bw = 10000; //in Megabits/s
         final long storage = 1000000; //in Megabytes
-        final ResourceProvisioner ramProvisioner = new ResourceProvisionerSimple();
-        final ResourceProvisioner bwProvisioner = new ResourceProvisionerSimple();
-        final VmScheduler vmScheduler = new VmSchedulerTimeShared();
+        final var vmScheduler = new VmSchedulerTimeShared();
 
-        final Host host = new HostSimple(ram, bw, storage, peList);
+        final var host = new HostSimple(ram, bw, storage, peList);
 
-        final PowerModelHost powerModel = new PowerModelHostSimple(MAX_POWER, STATIC_POWER);
+        final var powerModel = new PowerModelHostSimple(MAX_POWER, STATIC_POWER);
         powerModel.setStartupDelay(HOST_START_UP_DELAY)
                   .setShutDownDelay(HOST_SHUT_DOWN_DELAY)
                   .setStartupPower(HOST_START_UP_POWER)
                   .setShutDownPower(HOST_SHUT_DOWN_POWER);
 
-        host
-            .setRamProvisioner(ramProvisioner)
-            .setBwProvisioner(bwProvisioner)
-            .setVmScheduler(vmScheduler)
-            .setPowerModel(powerModel);
+        host.setVmScheduler(vmScheduler).setPowerModel(powerModel);
         host.setId(id);
         host.enableUtilizationStats();
 
@@ -298,9 +285,9 @@ public class PowerExample {
      * Creates a list of VMs.
      */
     private List<Vm> createVms() {
-        final List<Vm> list = new ArrayList<>(VMS);
+        final var list = new ArrayList<Vm>(VMS);
         for (int i = 0; i < VMS; i++) {
-            Vm vm = new VmSimple(i, 1000, VM_PES);
+            final var vm = new VmSimple(i, 1000, VM_PES);
             vm.setRam(512).setBw(1000).setSize(10000)
               .setCloudletScheduler(new CloudletSchedulerTimeShared());
             vm.enableUtilizationStats();
@@ -314,22 +301,21 @@ public class PowerExample {
      * Creates a list of Cloudlets.
      */
     private List<Cloudlet> createCloudlets() {
-        final List<Cloudlet> list = new ArrayList<>(CLOUDLETS);
-        final UtilizationModel utilization = new UtilizationModelDynamic(0.2);
+        final var cloudletList = new ArrayList<Cloudlet>(CLOUDLETS);
+        final var utilization = new UtilizationModelDynamic(0.2);
         for (int i = 0; i < CLOUDLETS; i++) {
             //Sets half of the cloudlets with the defined length and the other half with the double of it
             final long length = i < CLOUDLETS / 2 ? CLOUDLET_LENGTH : CLOUDLET_LENGTH * 2;
-            Cloudlet cloudlet =
+            final var cloudlet =
                 new CloudletSimple(i, length, CLOUDLET_PES)
                     .setFileSize(1024)
                     .setOutputSize(1024)
                     .setUtilizationModelCpu(new UtilizationModelFull())
                     .setUtilizationModelRam(utilization)
                     .setUtilizationModelBw(utilization);
-            list.add(cloudlet);
+            cloudletList.add(cloudlet);
         }
 
-        return list;
+        return cloudletList;
     }
-
 }
