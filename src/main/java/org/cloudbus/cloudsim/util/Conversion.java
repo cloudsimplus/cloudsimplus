64c03cab31e9d62a5057f3ffa170fedb8c8f4593
Manoel Campos <manoelcampos@gmail.com>
2023-03-10 11:34:31 -0300

    Updates Conversion docs
    Signed-off-by: Manoel Campos <manoelcampos@gmail.com>

--- src/main/java/org/cloudbus/cloudsim/util/Conversion.java
+++ src/main/java/org/cloudbus/cloudsim/util/Conversion.java
@@ -29,6 +29,7 @@ package org.cloudbus.cloudsim.util;
  * @author Manoel Campos da Silva Filho
  * @see TimeUtil
  * @see MathUtil
+ * @see BytesConversion
  */
 public final class Conversion {
     /**
