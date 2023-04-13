30e25b8ee28ea578b64fb9374996a4d26f023e2b
Manoel Campos <manoelcampos@gmail.com>
2023-04-13 10:58:40 -0300

    Version bump to 8.1.0
    Signed-off-by: Manoel Campos <manoelcampos@gmail.com>

--- src/main/java/org/cloudsimplus/core/CloudSim.java
+++ src/main/java/org/cloudsimplus/core/CloudSim.java
@@ -39,7 +39,7 @@ abstract class CloudSim implements Simulation {
     /**
      * CloudSim Plus current version.
      */
-    public static final String VERSION = "CloudSim Plus 8.0.0";
+    public static final String VERSION = "CloudSim Plus 8.1.0";
 
     public static final Logger LOGGER = LoggerFactory.getLogger(CloudSim.class.getSimpleName());
 
