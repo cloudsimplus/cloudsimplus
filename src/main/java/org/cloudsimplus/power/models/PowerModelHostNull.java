0fbe54bafdaacb55b92fc897d371c0d48109322a
Manoel Campos <manoelcampos@gmail.com>
2023-05-07 17:56:00 -0300

    Introduces PowerModelHostAbstract class and makes PowerModelHost an interface.
    Signed-off-by: Manoel Campos <manoelcampos@gmail.com>

--- src/main/java/org/cloudsimplus/power/models/PowerModelHostNull.java
+++ src/main/java/org/cloudsimplus/power/models/PowerModelHostNull.java
@@ -8,8 +8,20 @@ import org.cloudsimplus.power.PowerMeasurement;
  * {@link PowerModelHost} objects.
  * @since CloudSim Plus 6.0.0
  */
-class PowerModelHostNull extends PowerModelHost {
+class PowerModelHostNull implements PowerModelHost {
     @Override public PowerMeasurement getPowerMeasurement() { return new PowerMeasurement(); }
-    @Override public double getPowerInternal(double utilizationFraction) { return 0; }
+    @Override public double getPower(double utilizationFraction) { return 0; }
+    @Override public PowerModelHost setStartupPower(double power) { return this; }
+    @Override public PowerModelHost setShutDownPower(double power) { return this; }
+    @Override public void addStartupTotals() {/**/}
+    @Override public void addShutDownTotals() {/**/}
     @Override public Host getHost() { return Host.NULL; }
+    @Override public double getStartupPower() { return 0; }
+    @Override public double getShutDownPower() { return 0; }
+    @Override public double getTotalStartupPower() { return 0; }
+    @Override public double getTotalShutDownPower() { return 0; }
+    @Override public double getTotalStartupTime() { return 0; }
+    @Override public double getTotalShutDownTime()  { return 0; }
+    @Override public int getTotalStartups() { return 0; }
+    @Override public PowerModelHost setHost(Host host) { return this; }
 }
