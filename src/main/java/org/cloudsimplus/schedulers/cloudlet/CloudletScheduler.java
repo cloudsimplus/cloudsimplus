fabda6c3e4c68ddb77219dfc8398e40b6d48a237
Manoel Campos <manoelcampos@gmail.com>
2023-10-05 12:06:45 -0300

    Fix #460
    Signed-off-by: Manoel Campos <manoelcampos@gmail.com>

--- src/main/java/org/cloudsimplus/schedulers/cloudlet/CloudletScheduler.java
+++ src/main/java/org/cloudsimplus/schedulers/cloudlet/CloudletScheduler.java
@@ -270,7 +270,7 @@ public interface CloudletScheduler extends Serializable {
      *
      * @param currentTime current simulation time
      * @param mipsShare list with MIPS share of each Pe available to the scheduler
-     * @return the predicted completion time of the earliest finishing cloudlet
+     * @return the next time to update cloudlets processing
      * (which is a relative delay from the current simulation time),
      * or {@link Double#MAX_VALUE} if there is no next Cloudlet to execute
      */
