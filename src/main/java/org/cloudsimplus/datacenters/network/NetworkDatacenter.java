a7d2c9affb0f31f22a7915c2efed43c9104a8015
Manoel Campos <manoelcampos@gmail.com>
2025-04-29 18:51:31 -0300

    Updates NetworkDatacenter docs
    Signed-off-by: Manoel Campos <manoelcampos@gmail.com>

--- src/main/java/org/cloudsimplus/datacenters/network/NetworkDatacenter.java
+++ src/main/java/org/cloudsimplus/datacenters/network/NetworkDatacenter.java
@@ -9,6 +9,7 @@ package org.cloudsimplus.datacenters.network;
 
 import org.cloudsimplus.allocationpolicies.VmAllocationPolicy;
 import org.cloudsimplus.allocationpolicies.VmAllocationPolicySimple;
+import org.cloudsimplus.core.CloudSimPlus;
 import org.cloudsimplus.core.Simulation;
 import org.cloudsimplus.datacenters.Datacenter;
 import org.cloudsimplus.datacenters.DatacenterSimple;
@@ -16,6 +17,9 @@ import org.cloudsimplus.hosts.Host;
 import org.cloudsimplus.hosts.network.NetworkHost;
 import org.cloudsimplus.network.switches.EdgeSwitch;
 import org.cloudsimplus.network.switches.Switch;
+import org.cloudsimplus.resources.Pe;
+import org.cloudsimplus.vms.Vm;
+import org.cloudsimplus.vms.network.NetworkVm;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -24,11 +28,11 @@ import java.util.List;
 import static java.util.stream.Collectors.toList;
 
 /**
- * NetworkDatacenter class is a {@link Datacenter} whose hosts have network support.
- * It contains all the information about internal network.
- * For example, which VM is connected to what switch, etc.
+ * A {@link Datacenter} whose {@link Host}s have network support.
+ * It contains all the information about the internal network.
+ * For example, which {@link NetworkVm} is connected to which {@link Switch}, etc.
  *
- * <p>Please refer to following publication for more details:
+ * <p>Please refer to the following publication for more details:
  * <ul>
  * <li>
  * <a href="https://doi.org/10.1109/UCC.2011.24">
@@ -52,11 +56,11 @@ public class NetworkDatacenter extends DatacenterSimple {
     /**
      * Creates a NetworkDatacenter with the given parameters.
      *
-     * @param simulation The CloudSimPlus instance that represents the simulation the Entity belongs to
+     * @param simulation The {@link CloudSimPlus} instance that represents the simulation the Entity belongs to
      * @param hostList list of {@link Host}s that will compound the Datacenter
-     * @param vmAllocationPolicy the policy to be used to allocate VMs into hosts
+     * @param vmAllocationPolicy the policy to be used to allocate {@link Vm}s into hosts
      *
-     * @throws IllegalArgumentException when this Host has zero number of PEs (Processing Elements).
+     * @throws IllegalArgumentException when this Host has zero {@link Pe}s
      */
     public NetworkDatacenter(
         final Simulation simulation,
@@ -68,13 +72,12 @@ public class NetworkDatacenter extends DatacenterSimple {
     }
 
     /**
-     * Creates a NetworkDatacenter that uses a {@link VmAllocationPolicySimple}
-     * as default.
+     * Creates a NetworkDatacenter that uses a {@link VmAllocationPolicySimple} as default.
      *
-     * @param simulation The CloudSimPlus instance that represents the simulation the Entity belongs to
+     * @param simulation The {@link CloudSimPlus} instance that represents the simulation the Entity belongs to
      * @param hostList list of {@link Host}s that will compound the Datacenter
      *
-     * @throws IllegalArgumentException when this Host has zero number of PEs (Processing Elements).
+     * @throws IllegalArgumentException when this Host has zero {@link Pe}s
      */
     public NetworkDatacenter(
         final Simulation simulation,
@@ -85,11 +88,8 @@ public class NetworkDatacenter extends DatacenterSimple {
     }
 
     /**
-     * Gets a map of all Edge Switches in the Datacenter network, where each key is the switch id
-     * and each value is the switch itself.
-     * One can design similar functions for other type of Datacenter.
-     *
-     * @return
+     * @return a list of all {@link EdgeSwitch}s in the Datacenter network.
+     * @see #getSwitchMap()
      */
     public List<Switch> getEdgeSwitch() {
         return switchMap.stream()
@@ -106,9 +106,9 @@ public class NetworkDatacenter extends DatacenterSimple {
     }
 
     /**
-     * Gets a <b>read-only</b> list of network Datacenter's {@link Switch}es.
-     * @return
+     * @return a <b>read-only</b> list of network Datacenter's {@link Switch}es.
      */
+    // TODO This method and attribute must be renamed to switchList
     public List<Switch> getSwitchMap() {
         return Collections.unmodifiableList(switchMap);
     }
